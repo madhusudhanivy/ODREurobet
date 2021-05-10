fcc3384b4fcd2b6a006b3491457837ce5cc807af
madhusudhanivy <madhusudhan.putta@ivycomptech.com>
2021-05-10 22:26:45 +0530

    EmbeddedGame Added

--- Source/NativeUtils.swift
+++ Source/NativeUtils.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+typealias ThrowsCallback<T> = (() throws -> (T)) -> Void
+
+class NativeUtils: NSObject {
+  static func presentNative(_ vc: UIViewController, from parent: UIViewController? = nil, completion: (() -> Void)? = nil) {
+    DispatchQueue.main.async {
+      let parent = parent ?? UIApplication.shared.windows.first?.rootViewController
+      parent?.present(vc, animated: false, completion: completion)
+    }
+  }
+
+  static func closeAll() {
+    let parent = UIApplication.shared.windows.first?.rootViewController
+    parent?.navigationController?.popToRootViewController(animated: true)
+  }
+
+  static func closeFirst() {
+    UIApplication.shared.windows.first?.rootViewController?.dismiss(animated: true)
+  }
+
+}
