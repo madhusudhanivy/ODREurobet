4f0fd56c12a628a7c42f044087c33b55b2536c48
madhusudhanivy <madhusudhan.putta@ivycomptech.com>
2021-05-10 22:31:48 +0530

    ODR Manageer files added

---
+++ Source/GameManager.swift
@@ -0,0 +1,84 @@
+//
+//  GameManager.swift
+//  ODREurobet
+//
+//  Created by Madhusudhan Reddy Putta on 10/05/21.
+//
+
+import Foundation
+
+typealias ThrowableCallback<T> = (() throws -> (T)) -> Void
+
+class GameManager: NSObject {
+  
+  // MARK: - Singleton
+  static var shared = GameManager()
+  override private init() {
+    super.init()
+  }
+  
+  
+  // MARK: - Properties
+  var odr = ODRManager.shared
+  var zip = ZipManager.shared
+  var web = WebServerManager.shared
+  
+  var currentGame: String?
+  var webServerIndexFile: String? {didSet{web.webServerIndexFile=webServerIndexFile}}
+  var webServerPort: UInt? {didSet{web.port=webServerPort ?? 8080}}
+  var queryString: String?
+  var isForReal: Bool?
+  
+  var rctResolver: RCTPromiseResolveBlock?
+  var rctRejecter: RCTPromiseRejectBlock?
+  
+  
+  // MARK: - Methods
+  func requestUnzippedResource(_ tag: String, resolver: @escaping ThrowableCallback<String>) {
+    
+    // check if resource is already unzipped
+    if let resourceUnzippedPath = zip.resourceExists(tag) {
+      resolver({ return resourceUnzippedPath })
+      return
+    }
+    
+    // Request resource with odr
+    odr.requestResourceWith(tag) { zipPathThrowable in
+      if let zipPath = try? zipPathThrowable() {
+        self.zip.extractGame(zipPath, resolver: resolver)
+        return
+      }
+      
+      resolver({ throw GameViewController.CustomErrors.genericError })
+    }
+  }
+  
+  
+  func getCurrentGameUrl() -> String? {
+    guard let _ = currentGame, let _ = webServerPort, let gameLaunchUrl = queryString else { return nil }
+    return gameLaunchUrl
+  }
+}
+
+
+// MARK: - F***ing GamesCore is unzipped as ProjectTemplate
+extension GameManager {
+  func requestGamesCore(resolver: @escaping ThrowableCallback<String>) {
+    
+    // check if resource is already unzipped
+    if let resourceUnzippedPath = zip.gamesCoreExists() {
+      resolver({ return resourceUnzippedPath })
+      return
+    }
+    
+    // Request resource with odr
+    odr.requestResourceWith("GamesCore") { zipPathThrowable in
+      if let zipPath = try? zipPathThrowable() {
+        self.zip.extractGame(zipPath, resolver: resolver)
+        return
+      }
+      
+      resolver({ throw GameViewController.CustomErrors.genericError })
+    }
+  }
+}
---
+++ Source/ODRManager.swift
@@ -0,0 +1,57 @@
+//
+//  ODRManager.swift
+//  ODREurobet
+//
+//  Created by Madhusudhan Reddy Putta on 10/05/21.
+//
+
+import Foundation
+
+class ODRManager {
+  
+  // MARK: - Properties
+  var currentRequest: NSBundleResourceRequest = NSBundleResourceRequest(tags: [])
+  var progress: Progress {
+    return currentRequest.progress
+  }
+  
+  
+  // MARK: - Singleton
+  static let shared = ODRManager()
+  private init() { }
+  
+  
+  // MARK: - Methods
+  func requestResourceWith(_ tag: String, resolver: @escaping ThrowableCallback<String>) {
+    // So just go like this:
+    // Test; Search for this comment line and toggle comments below
+//    local(tag, resolver: resolver); return;
+    download(tag, resolver: resolver)
+  }
+  
+  func local(_ tag: String, resolver: @escaping ThrowableCallback<String>) {
+    resolver({ return Bundle.main.path(forResource: tag, ofType: "zip")! });
+  }
+}
+
+
+// MARK: - ODR Download Logic
+extension ODRManager {
+  func download(_ tag: String, resolver: @escaping ThrowableCallback<String>) {
+    currentRequest = NSBundleResourceRequest(tags: [tag])
+    
+    currentRequest.beginAccessingResources { error in
+      if let _ = error {
+        resolver({ throw GameViewController.CustomErrors.networkError })
+        return
+      }
+      
+      guard let resourcePath = self.currentRequest.bundle.path(forResource: tag, ofType: "zip") else {
+        resolver({ throw GameViewController.CustomErrors.notFoundError })
+        return
+      }
+      resolver({ return resourcePath })
+      self.currentRequest.endAccessingResources()
+    }
+  }
+}
