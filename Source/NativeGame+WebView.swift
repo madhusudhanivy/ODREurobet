415a08709a4c5ab4e84c712cd3e1a8257666984f
madhusudhanivy <madhusudhan.putta@ivycomptech.com>
2021-05-12 12:39:13 +0530

    Commented the duplicate source code

--- Source/NativeGame+WebView.swift
+++ Source/NativeGame+WebView.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import WebKit
 
 extension NativeGame {
   func getWKWebView() -> WKWebView {
--- Source/WebServerManager.swift
+++ Source/WebServerManager.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import WebKit
+import GCDWebServer
 
 @objc(WebServerManager)
 class WebServerManager: NSObject {
